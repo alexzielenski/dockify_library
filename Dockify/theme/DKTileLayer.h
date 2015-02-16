c84a6b9d7a4504872f87a0bcffbf74f4b3228c14
Alexander Zielenski <alex@alexzielenski.com>
2015-02-15 21:03:38 -0500

    add dktilelayer

---
+++ Dockify/theme/DKTileLayer.h
@@ -0,0 +1,13 @@
+//
+//  DKTileLayer.h
+//  Dockify
+//
+//  Created by Alexander Zielenski on 2/15/15.
+//  Copyright (c) 2015 Alexander Zielenski. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface DKTileLayer : CALayer
+
+@end
---
+++ Dockify/theme/DKTileLayer.m
@@ -0,0 +1,13 @@
+//
+//  DKTileLayer.m
+//  Dockify
+//
+//  Created by Alexander Zielenski on 2/15/15.
+//  Copyright (c) 2015 Alexander Zielenski. All rights reserved.
+//
+
+#import "DKTileLayer.h"
+
+@implementation DKTileLayer
+
+@end
